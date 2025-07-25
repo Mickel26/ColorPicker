1a0c72c4703ab0536c47169d9638f2a72ebb38fa
Mickel26 <mikolajfilka@gmail.com>
2025-07-25 18:40:45 +0200

    New icons

--- constants/icons.ts
+++ constants/icons.ts
@@ -1,5 +1,9 @@
+import home from "@/assets/icons/home.png";
 import plus from "@/assets/icons/plus.png";
+import settings from "@/assets/icons/settings.png";
 
 export const icons = {
-    plus
+    plus,
+    home,
+    settings
 }
